eaf9c8c5b46f7363cb9a10ec2afce8bd6b736b6f
iossocket <avx302@gmail.com>
2017-06-26 09:53:58 +0800

    1.0

---
+++ A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,16 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by XueliangZhu on 23/06/2017.
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+
+#import <CTMediator/CTMediator.h>
+
+@interface CTMediator (A)
+
+- (UIViewController *)A_aViewController;
+
+@end
---
+++ A_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A_Category
+//
+//  Created by XueliangZhu on 23/06/2017.
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
